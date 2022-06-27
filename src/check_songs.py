68bb491bf08a038fe1eb14c9d724f52f8d3de9cc
Michael Xie <mxxie@student.unimelb.edu.au>
2022-06-27 17:50:21 +1000

    Made sure capitalization of filenames is correct.

--- src/check_songs.py
+++ src/check_songs.py
@@ -9,23 +9,34 @@ from collections import Counter
 
 INDENT = '\n\t'
 
+def _findFileCaseSensitive(findIn: str, filename: str) -> tuple[list[str], list[str]]:
+    files = glob.glob(findIn + glob.escape(filename))
+    parents = set(Path(file).parent for file in files) 
+    getFilenames = lambda p: map(lambda c: c.name, p.glob("*")) # get case-correct list of all files for a folder p
+    valid_parents = set(filter(lambda parent: filename in getFilenames(parent), parents))
+    return list(map(lambda p: str(p/filename), valid_parents)), list(map(lambda p: str(p), parents - valid_parents))
 
 def _checkFiles(lines: list[str]) -> bool:
     flag = False
     for songname in lines:
-        folders = glob.glob('./DDR*/' + glob.escape(songname))
+        # folders = glob.glob('./DDR*/' + glob.escape(songname))
+        folders, invalid_parents = _findFileCaseSensitive(findIn = './DDR*/', filename = songname)
 
         if len(folders) > 1:
             LOGGER.warning('Duplicates in files:' + INDENT + INDENT.join(folders))
         if len(folders) == 0:
-            LOGGER.error("File not found:" + INDENT + songname)
+            msg = "File not found:" + INDENT + songname
+            if invalid_parents:
+                msg += INDENT + "but found in " + ",".join(invalid_parents)
+            LOGGER.error(msg)
+
             flag = True
 
     return flag
 
 def _checkDupesInList(lines: list[str]) -> bool:
     flag = False
-    counter = Counter(lines)
+    counter = Counter(line.lower() for line in lines)
     for k,v in counter.items():
         if v > 1:
             flag = True
