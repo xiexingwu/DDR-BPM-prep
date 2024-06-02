9fc209fd5bdfa99487bb31be8133c972882c2efa
Michael Xie <xie.michael.x@gmail.com>
2024-06-02 21:19:35 +1000

    Allow predeploy-force target

--- Makefiles/deploy.mk
+++ Makefiles/deploy.mk
@@ -2,19 +2,5 @@
 predeploy:
 	bash $(PROJ_DIR)/scripts/deploy/predeploy.sh
 
-
-# deploy: lowres
-# 	git push
-# 	git tag vAuto --force
-# 	git push origin vAuto --force
-#
-# zip: hash
-# 	zip data.zip -qr data || [ $$? -eq 12 ]
-# 	zip jackets.zip -qr jackets-lowres || [ $$? -eq 12 ]
-# 	zip simfiles.zip -qr simfiles || [ $$? -eq 12 ]
-#
-# hash:
-# 	bash ./bin/hashSongs.sh > hashed_songs.txt
-# 	bash ./bin/hashJackets.sh > hashed_jackets.txt
-# 	bash ./bin/hash.sh courses.json > hashed_courses.txt
-
+predeploy-force: export FORCE=Y
+predeploy-force: predeploy
--- scripts/deploy/predeploy.sh
+++ scripts/deploy/predeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SEED_DIR=${SEED_DIR:-./data}
-BUILD_DIR=${BUILD_DIR-./build}
+BUILD_DIR=${BUILD_DIR:-./build}
 
 jackets_dir=$BUILD_DIR/jackets-160
 mkdir -p $jackets_dir
@@ -8,8 +8,10 @@ mkdir -p $jackets_dir
 OIFS="$IFS"
 IFS=$'\n'
 while IFS= read -r name; do
-    name=$(echo $name | sed -e 's:\[:\\[:g' -e 's:]:\\]:g') # escape square brackets
-    png=$(find $SEED_DIR -type f -name "$name-jacket.png")
+    [[ -f "$jackets_dir/$name.png" && -z $FORCE ]] && continue
+
+    search_name=$(echo $name | sed -e 's:\[:\\[:g' -e 's:]:\\]:g') # escape square brackets
+    png=$(find $SEED_DIR -type f -name "$search_name-jacket.png")
     [[ -z $png || ${#png[@]} -eq 0 ]] && echo "$name jacket not found" && exit 1
     [[ ${#png[@]} -gt 1 ]] && echo "Multiple found for $name:\n\t$png" && exit 1
 
