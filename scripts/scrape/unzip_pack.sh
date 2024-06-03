fc5df7a21300f2568a28cb0f684cb3a72a02fb8d
Michael Xie <xie.michael.x@gmail.com>
2024-06-04 00:14:09 +1000

    Hide unzip warnings

--- scripts/scrape/unzip_pack.sh
+++ scripts/scrape/unzip_pack.sh
@@ -2,6 +2,5 @@
 SEED_DIR=${SEED_DIR:-./data}
 
 for f in $SEED_DIR/*.zip; do
-    # zip -d $f "*.avi" "*.ogg" || [ $? -eq 12 ];
-    unzip -uo "$f" "*.sm" "*.ssc" "*.png" -d "$(echo $f | sed -e 's/\.zip//')" || [ $? -eq 9 ];
+    unzip -uo "$f" "*.sm" "*.ssc" "*.png" -d "$(echo $f | sed -e 's/\.zip//')" || [ $? -eq 1 || $? -eq 9 ];
 done
