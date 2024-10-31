395999b9f664a52d9a6a97dc3ac3be62aec7fb0b
Michael Xie <xie.michael.x@gmail.com>
2024-11-01 09:54:51 +1100

    bash [ to [[

--- scripts/scrape/unzip_pack.sh
+++ scripts/scrape/unzip_pack.sh
@@ -2,5 +2,5 @@
 SEED_DIR=${SEED_DIR:-./data}
 
 for f in $SEED_DIR/*.zip; do
-    unzip -uo "$f" "*.sm" "*.ssc" "*.png" -d "$(echo $f | sed -e 's/\.zip//')" || [ $? -eq 1 || $? -eq 9 ];
+    unzip -uo "$f" "*.sm" "*.ssc" "*.png" -d "$(echo $f | sed -e 's/\.zip//')" || [[ $? -eq 1 || $? -eq 9 ]];
 done
