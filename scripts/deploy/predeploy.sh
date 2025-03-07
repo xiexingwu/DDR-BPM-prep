9f5d783e5ed5e9ce32e03d9828964ac1b851132d
Michael Xie <xie.michael.x@gmail.com>
2025-03-07 23:10:11 +1100

    predeploy now prepares artifacts

--- scripts/deploy/predeploy.sh
+++ scripts/deploy/predeploy.sh
@@ -2,6 +2,7 @@
 SEED_DIR=${SEED_DIR:-./data}
 BUILD_DIR=${BUILD_DIR:-./build}
 
+# Downsize images
 jackets_dir=$BUILD_DIR/jackets-160
 mkdir -p $jackets_dir
 
@@ -15,6 +16,15 @@ while IFS= read -r name; do
     [[ -z $png || ${#png[@]} -eq 0 ]] && echo "$name jacket not found" && exit 1
     [[ ${#png[@]} -gt 1 ]] && echo "Multiple found for $name:\n\t$png" && exit 1
 
+    # Images
     echo Downsizing $name from $png
     convert "$png" -resize 160x160 -quality 100 "$jackets_dir/$name.png"
 done < $SEED_DIR/all_songs.txt
+
+
+# Zip
+7z a -tzip $BUILD_DIR/songs.zip -w $BUILD_DIR/songs/.
+7z a -tzip $BUILD_DIR/jackets.zip -w $jackets_dir/.
+
+# other files
+cp $SEED_DIR/all_songs.txt $SEED_DIR/removed.txt $BUILD_DIR/
